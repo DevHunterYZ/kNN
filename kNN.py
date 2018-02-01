bd9cab5553984dd8019a67ecc22a2d81e8e6765e
Berkay <32465888+DevHunterYZ@users.noreply.github.com>
2018-02-02 01:05:34 +0300

    Update kNN.py

--- kNN.py
+++ kNN.py
@@ -1,18 +1,17 @@
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-from array import array
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
 nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
 distances, indices = nbrs.kneighbors(X)
 indices                                           
-array([[0, 1],
+a = np.array([[0, 1],
        [1, 0],
        [2, 1],
        [3, 4],
        [4, 3],
        [5, 4]])
 distances
-array([[ 0.        ,  1.        ],
+b = np.array([[ 0.        ,  1.        ],
        [ 0.        ,  1.        ],
        [ 0.        ,  1.41421356],
        [ 0.        ,  1.        ],
